5b329126e69b4b512543fb88a7b858833e4f683f
antiftw <atjanssen@hotmail.nl>
2024-06-13 21:33:32 +0200

    some mismatching function declarations

--- src/Api/ProtectedContentExtension.php
+++ src/Api/ProtectedContentExtension.php
@@ -7,6 +7,7 @@ namespace Bolt\UsersExtension\Api;
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
 use Bolt\Configuration\Config;
 use Bolt\Entity\Content;
 use Bolt\Entity\Field;
@@ -33,7 +34,7 @@ final class ProtectedContentExtension implements QueryCollectionExtensionInterfa
         }
     }
 
-    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGenerator|\ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?string $operationName = null): void
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?Operation $operation = null, array $context = []): void
     {
         if ($resourceClass === Content::class) {
             $this->filterProtectedContent($queryBuilder);
@@ -44,7 +45,7 @@ final class ProtectedContentExtension implements QueryCollectionExtensionInterfa
         }
     }
 
-    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, ?string $operationName = null, array $context = []): void
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, ?Operation $operation = null, array $context = []): void
     {
         if ($resourceClass === Content::class) {
             $this->filterProtectedContent($queryBuilder);
